72e0ca8919c5c5954bcc8bae382121d756aef32d
MahuyaP25 <mahuyapandey2004@gmail.com>
2026-02-18 09:35:11 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'Generative AI Project Demo',
+    version = '0.0.0',
+    author = 'Mahuya Pandey',
+    author_email= 'mahuyapandey2004@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
